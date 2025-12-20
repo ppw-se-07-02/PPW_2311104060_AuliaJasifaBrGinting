9e4df7cca1b110244761aa1f1c9391270775dad8
“auliajsf06” <auliajasifa24@gmail.com>
2025-12-20 18:46:04 +0700

    Menambahkan Praktikum 11

---
+++ PERTEMUAN11/praktikum/resources/views/mahasiswa.blade.php
@@ -0,0 +1,31 @@
+<!-- <!DOCTYPE html> 
+<html> 
+    <head> 
+        <title>Belajar Laravel</title> 
+    </head> 
+    <body> 
+        <h2> Data Mahasiswa </h2> 
+        <ol> 
+            <li>Mahasiswa 1</li> 
+            <li>Mahasiswa 2</li> 
+            <li>Mahasiswa 3</li> 
+            <li>Mahasiswa 4</li> 
+        </ol> 
+    </body> 
+</html>  -->
+
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <title>Belajar Laravel</title> 
+    </head> 
+    <body> 
+        <h2> Data Mahasiswa </h2> 
+        <ol> 
+            <li><?php echo $mhs1; ?></li> 
+            <li><?php echo $mhs2; ?></li> 
+            <li><?php echo $mhs3; ?></li> 
+            <li><?php echo $mhs4; ?></li> 
+        </ol>  
+    </body> 
+</html> 
